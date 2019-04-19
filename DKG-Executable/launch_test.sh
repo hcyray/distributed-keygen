8d6ba4b357bdbc54797df23573627117019a6a21
hcyray <chuangak@connect.ust.hk>
2019-04-19 21:24:34 +0800

    Add files via upload

---
+++ DKG-Executable/launch_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function launch () {
+    port=$1
+    id=$2
+    ./node_8_0 $port certs/$id.pem certs/$id-key.pem contlist 0 0 0
+}
+
+for x in `seq 1 70`
+do
+    rm -r node$x
+    mkdir node$x
+    ln -s $PWD/certs node$x/
+    ln -s $PWD/system.param node$x/
+    ln -s $PWD/pairing.param node$x/
+done
+
+a=9000
+for x in `seq 1 70`
+do
+    cd node$x
+    ((a++))
+    ../node_8_0 $a ../certs/$x.pem ../certs/$x-key.pem ../contlist 0 0 0 &
+    cd ..
+done
+tail -F node1/message.log
+
